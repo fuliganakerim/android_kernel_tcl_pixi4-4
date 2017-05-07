f01ebf5198b8d7a850982d9b5538f84b2154f92f
kirito96 <aquammie96@gmail.com>
2017-05-07 02:46:21 -0800

    Update buildkernelTravis.sh

--- buildkernelTravis.sh
+++ buildkernelTravis.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Get kernel configuration
-if [ -f kernel.conf ]
-  then
-    source "kernel.conf"
-  else
-	echo "Kernel configuration file (kernel.conf) does not exist!"
-	exit -1
-fi
-
 export USE_CCACHE=1
 export ARCH=arm ARCH_MTK_PLATFORM=mt6580
 #make clean 
